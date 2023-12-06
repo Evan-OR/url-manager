bfc8125dccb38f1a49ce7ff9e8f6b86fbea7801c
18EOR <42158152+18EOR@users.noreply.github.com>
2023-12-06 23:32:34 +0000

    added boilerplate client side routing with react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import RegisterForm from './components/RegisterForm/RegisterForm';
 import Cookies from 'js-cookie';
 import { User } from './utils/types';
+import HomePage from './components/HomePage/HomePage';
+import ManageUrls from './components/ManageUrls/ManageUrls';
+import URLAnalytics from './components/URLAnalytics/URLAnalytics';
 
 function App() {
     const [data, setData] = useState(null);
@@ -20,19 +25,27 @@ function App() {
     }, []);
 
     return (
-        <>
-            {user ? (
-                <div>helle {user.username}</div>
-            ) : (
-                <>
-                    <button onClick={getData}>Get Data</button>
-                    {data && <div>{JSON.stringify(data)}</div>}
-
-                    <RegisterForm setUser={setUser} />
-                </>
-            )}
-        </>
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/auth" element={<RegisterForm setUser={setUser} />} />
+            <Route path="/manage" element={<ManageUrls />} />
+            <Route path="/analytics" element={<URLAnalytics />} />
+        </Routes>
     );
+    // return (
+    //     <>
+    //         {user ? (
+    //             <div>helle {user.username}</div>
+    //         ) : (
+    //             <>
+    //                 <button onClick={getData}>Get Data</button>
+    //                 {data && <div>{JSON.stringify(data)}</div>}
+
+    //                 <RegisterForm setUser={setUser} />
+    //             </>
+    //         )}
+    //     </>
+    // );
 }
 
 export default App;
